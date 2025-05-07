0a37ab037f462990f4a7e56a3400cab5f23b50e9
Vianne Häfeli <vianne.haefeli@gmail.com>
2025-05-07 15:30:14 +0200

    test

--- script.js
+++ script.js
@@ -2,38 +2,42 @@ let map = L.map('map').setView([47.0551347757616, 8.305066129833179], 16);
 
 let green_marker = L.icon({
     iconUrl: 'img/green_marker.svg',
+    iconSize:     [38, 95],
+    iconAnchor: [19, 75]
 });
 
 let orange_marker = L.icon({
     iconUrl: 'img/orange_marker.svg',
+    iconSize:     [38, 95],
 });
 
 let red_marker = L.icon({
     iconUrl: 'img/red_marker.svg',
+    iconSize:     [38, 95],
 });
 
 // OpenStreetMap-Tiles laden
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  attribution: 'CartoDB Dark Matter'
 }).addTo(map);
 
 // Marker mit Popup hinzufügen
-L.marker([47.05846993506582, 8.310923229170475], {icon: green_marker}).addTo(map)
+L.marker([47.058341871115616, 8.310841259645938], {icon: green_marker}).addTo(map)
   .bindPopup('Löwendenkmal')
   .openPopup();
 
-  L.marker([47.05549754312702, 8.310256739922227]).addTo(map)
+  L.marker([47.05549754312702, 8.310256739922227], {icon: green_marker}).addTo(map)
   .bindPopup('Hertensteinstrasse')
   .openPopup();
 
-  L.marker([47.053680374103024, 8.308729270948183]).addTo(map)
+  L.marker([47.05361289724891, 8.308787900487744], {icon: green_marker}).addTo(map)
   .bindPopup('Schwanenplatz')
   .openPopup();
 
-  L.marker([47.05233432536328, 8.307978424218748]).addTo(map)
+  L.marker([47.05233432536328, 8.307978424218748], {icon: green_marker}).addTo(map)
   .bindPopup('Rathausquai')
   .openPopup();
 
-  L.marker([47.0516, 8.3075]).addTo(map)
+  L.marker([47.0516, 8.3075], {icon: green_marker}).addTo(map)
   .bindPopup('Kapellbrücke')
   .openPopup();
