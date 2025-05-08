13dd5383916367334ff61b91b1701e7caacb281b
Giannamohler <gianna.mohler@stud.fhgr.ch>
2025-05-08 10:00:30 +0200

    popups neu

--- script.js
+++ script.js
@@ -25,36 +25,16 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
 
 // Marker mit Popup hinzufügen
 L.marker([47.058341871115616, 8.310841259645938], {icon: green_marker}).addTo(map)
-  .bindPopup('Löwendenkmal')
-  .openPopup();
+  .bindPopup('Löwendenkmal <br> Mehr Platz als während dem Corona-Lockdown <br> Anzahl Leute: 22 <br> zuletzt gemessen: 6:58');
 
   L.marker([47.05549754312702, 8.310256739922227], {icon: green_marker}).addTo(map)
-  .bindPopup('Hertensteinstrasse')
-  .openPopup();
+  .bindPopup('Hertensteinstrasse <br> Mehr Platz als während dem Corona-Lockdown <br> Anzahl Leute: 22 <br> zuletzt gemessen: 6:58');
 
   L.marker([47.05361289724891, 8.308787900487744], {icon: green_marker}).addTo(map)
-  .bindPopup('Schwanenplatz')
-  .openPopup();
+  .bindPopup('Schwanenplatz <br> Mehr Platz als während dem Corona-Lockdown <br> Anzahl Leute: 22 <br> zuletzt gemessen: 6:58');
 
   L.marker([47.05233432536328, 8.307978424218748], {icon: green_marker}).addTo(map)
-  .bindPopup('Rathausquai')
-  .openPopup();
+  .bindPopup('Rathausquai <br> Mehr Platz als während dem Corona-Lockdown <br> Anzahl Leute: 22 <br> zuletzt gemessen: 6:58');
 
   L.marker([47.0516, 8.3075], {icon: green_marker}).addTo(map)
-  .bindPopup('<p class="test">Hello world!<br />This is a nice popup.</p>')
-
-
-  const bubble = L.divIcon({
-    className: 'speech-bubble',
-    iconAnchor: [19, 75]
-  });
-  
-  L.marker([47.05796842786666, 8.308897547272052], {icon: bubble }).addTo(map);
-
-  L.marker([47.05521471908057, 8.306938713116102], {icon: bubble}).addTo(map);
-
-  L.marker([47.05325135356975, 8.303726564553916], {icon: bubble}).addTo(map);
-
-  L.marker([47.05147643654085, 8.298668482793298], {icon: bubble}).addTo(map);
-
-  L.marker([47.05033486219313, 8.304265147512542], {icon: bubble}).addTo(map);
+  .bindPopup('Kapellbrücke <br> Mehr Platz als während dem Corona-Lockdown <br> Anzahl Leute: 22 <br> zuletzt gemessen: 6:58');
