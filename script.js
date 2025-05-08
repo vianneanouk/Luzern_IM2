b133dde5a2f1f9b71252611b8cb56b2b914a0f61
Vianne Häfeli <vianne.haefeli@gmail.com>
2025-05-08 12:10:37 +0200

    time und counter

--- script.js
+++ script.js
@@ -34,8 +34,30 @@ function renderSensors(sensors) {
 
 const data = await fetchSensorData(API_URL);
 console.log(data);
-let loewendenkmal = data[0];
+let hertensteinstrasse = data[0];
+console.log(hertensteinstrasse)
+
+const data1 = await fetchSensorData(API_URL);
+console.log(data);
+let kapellbruecke = data[1];
+console.log(kapellbruecke)
+
+const data3 = await fetchSensorData(API_URL);
+console.log(data);
+let loewendenkmal = data[3];
 console.log(loewendenkmal)
+
+const data4 = await fetchSensorData(API_URL);
+console.log(data);
+let rathausquai = data[4];
+console.log(rathausquai)
+
+const data5 = await fetchSensorData(API_URL);
+console.log(data);
+let schwanenplatz = data[5];
+console.log(schwanenplatz)
+
+
 
 let map = L.map('map').setView([47.0551347757616, 8.305066129833179], 16);
 
@@ -102,34 +124,41 @@ L.marker([47.058341871115616, 8.310841259645938], {icon: green_marker}).addTo(ma
 .bindPopup(`
   <div class="popup-content">
     <h2>Mehr Platz als während dem Corona-Lockdown</h2>
-    <p class="info">Anzahl Leute: 22<br>zuletzt gemessen: 6:58 Uhr</p>
+    <p class="info">Anzahl Leute: ${loewendenkmal.counter}<br>zuletzt gemessen: ${loewendenkmal.time} Uhr</p>
     </div>`);
 
-L.marker([47.0580694107522, 8.311013370717475], { icon: loewendenkmalIcon }).addTo(map);
+L.marker([47.0580694107522, 8.311013370717475], {icon: loewendenkmalIcon }).addTo(map);
+
+
+
 
 L.marker([47.05549754312702, 8.310256739922227], {icon: green_marker}).addTo(map)
 .bindPopup(`
   <div class="popup-content">
     <h2>Mehr Platz als während dem Corona-Lockdown</h2>
-    <p class="info">Anzahl Leute: 22<br>zuletzt gemessen: 6:58 Uhr</p>
+    <p class="info">Anzahl Leute: ${hertensteinstrasse.counter} <br>zuletzt gemessen: ${hertensteinstrasse.time} Uhr</p>
     </div>`);
 
 L.marker([47.05520389812051, 8.31030314438318], { icon: hertensteinstrasseIcon }).addTo(map);
+
+
 
 L.marker([47.05361289724891, 8.308787900487744], {icon: green_marker}).addTo(map)
 .bindPopup(`
   <div class="popup-content">
     <h2>Mehr Platz als während dem Corona-Lockdown</h2>
-    <p class="info">Anzahl Leute: 22<br>zuletzt gemessen: 6:58 Uhr</p>
+    <p class="info">Anzahl Leute: ${schwanenplatz.counter}<br>zuletzt gemessen: ${schwanenplatz.time} Uhr</p>
     </div>`);
 
-L.marker([47.053326495141825, 8.308983862796921], { icon: schwanenplatzIcon }).addTo(map);
+L.marker([47.053326495141825, 8.308983862796921], {icon: schwanenplatzIcon }).addTo(map);
+
+
 
 L.marker([47.05233432536328, 8.307978424218748], {icon: green_marker}).addTo(map)
 .bindPopup(`
   <div class="popup-content">
     <h2>Mehr Platz als während dem Corona-Lockdown</h2>
-    <p class="info">Anzahl Leute: 22<br>zuletzt gemessen: 6:58 Uhr</p>
+    <p class="info">Anzahl Leute: ${rathausquai.counter}<br>zuletzt gemessen: ${rathausquai.time} Uhr</p>
     </div>`);
 
 L.marker([47.052037509036495, 8.309074616902045], { icon: rathausquaiIcon }).addTo(map);
@@ -138,10 +167,7 @@ L.marker([47.0516, 8.3075], {icon: green_marker}).addTo(map)
 .bindPopup(`
   <div class="popup-content">
     <h2>Mehr Platz als während dem Corona-Lockdown</h2>
-    <p class="info">Anzahl Leute: 22<br>zuletzt gemessen: 6:58 Uhr</p>
+    <p class="info">Anzahl Leute: ${kapellbruecke.counter}<br>zuletzt gemessen: ${kapellbruecke.time} Uhr</p>
     </div>`);
 
   L.marker([47.05141583560954, 8.30709768417738], { icon: kapellbrueckeIcon }).addTo(map);
-
-
-  
