11cad0136ecd7d54e9b629c6148ee37cfb1b1044
Giannamohler <gianna.mohler@stud.fhgr.ch>
2025-05-26 11:48:14 +0200

    kommentare

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector("#sensor-container");
 
 const API_URL = "https://portal.alfons.io/app/devicecounter/api/sensors?api_key=3ad08d9e67919877e4c9f364974ce07e36cbdc9e";
 
+// Funktion zum Abrufen der Sensordaten von der API
 async function fetchSensorData(url) {
     try {
         const response = await fetch(url);
@@ -19,7 +20,7 @@ async function fetchSensorData(url) {
     }
 }
 
-
+// Funktion zum Darstellen der Sensordaten im HTML
 function renderSensors(sensors) {
     sensors.forEach(sensor => {
         const div = document.createElement("div");
@@ -32,6 +33,7 @@ function renderSensors(sensors) {
     });
 }
 
+//API-Daten abrufen und in Variablen speichern
 const data = await fetchSensorData(API_URL);
 console.log("Sensor-Daten:", data);
 
@@ -43,11 +45,10 @@ let schwanenplatz = data[5];
 
 console.log({ hertensteinstrasse, kapellbruecke, loewendenkmal, rathausquai, schwanenplatz });
 
-
-
-
+//Leaflet-Karte initialisieren (Zentrum Luzern)
 let map = L.map('map').setView([47.0551347757616, 8.305066129833179], 16);
 
+//Marker-Icons Farben definieren
 let green_marker = L.icon({
     iconUrl: 'img/green_marker.svg',
     iconSize:     [38, 95],
@@ -66,46 +67,45 @@ let red_marker = L.icon({
     iconAnchor: [19, 75],
 });
 
+// Text-Icons (Labels) für die Orte definieren
 let loewendenkmalIcon = L.divIcon({
-  className: 'text-label', // optional für eigenes CSS
+  className: 'text-label',
   html: 'Löwendenkmal',
-  iconSize: [100, 40], // Größe des "Icons" (optional)
-  iconAnchor: [50, 20]  // Ankerpunkt, damit es gut sitzt
+  iconSize: [100, 40], 
+  iconAnchor: [50, 20],  
 });
 
 let hertensteinstrasseIcon = L.divIcon({
-  className: 'text-label', // optional für eigenes CSS
+  className: 'text-label', 
   html: 'Hertensteinstrasse',
-  iconSize: [100, 40], // Größe des "Icons" (optional)
-  iconAnchor: [50, 20]  // Ankerpunkt, damit es gut sitzt
+  iconSize: [100, 40], 
+  iconAnchor: [50, 20],
 });
 
 let schwanenplatzIcon = L.divIcon({
-  className: 'text-label', // optional für eigenes CSS
+  className: 'text-label',
   html: 'Schwanenplatz',
-  iconSize: [100, 40], // Größe des "Icons" (optional)
-  iconAnchor: [50, 20]  // Ankerpunkt, damit es gut sitzt
+  iconSize: [100, 40],
+  iconAnchor: [50, 20],
 });
 
 let rathausquaiIcon = L.divIcon({
-  className: 'text-label', // optional für eigenes CSS
+  className: 'text-label',
   html: 'Rathausquai',
-  iconSize: [100, 40], // Größe des "Icons" (optional)
-  iconAnchor: [50, 20]  // Ankerpunkt, damit es gut sitzt
+  iconSize: [100, 40],
+  iconAnchor: [50, 20],
 });
 
 let kapellbrueckeIcon = L.divIcon({
-  className: 'text-label', // optional für eigenes CSS
+  className: 'text-label',
   html: 'Kapellbrücke',
-  iconSize: [100, 40], // Größe des "Icons" (optional)
-  iconAnchor: [50, 20]  // Ankerpunkt, damit es gut sitzt
+  iconSize: [100, 40],
+  iconAnchor: [50, 20],
 });
 
-
-
-
+//Funktion zur Formatierung der Zeitangabe
 function formatUnixTimeToHHMM(isoTime) {
-  const date = new Date(isoTime);  // Direktes Umwandeln des ISO-Zeitstrings
+  const date = new Date(isoTime); 
   if (isNaN(date)) return 'Keine Zeit verfügbar';
 
   return date.toLocaleTimeString('de-CH', {
@@ -120,7 +120,7 @@ hertensteinstrasse.time = formatUnixTimeToHHMM(hertensteinstrasse.ISO_time);
 rathausquai.time = formatUnixTimeToHHMM(rathausquai.ISO_time);
 schwanenplatz.time = formatUnixTimeToHHMM(schwanenplatz.ISO_time);
 
-
+//Besucherzahlen in Text umwandeln
 function getCrowdMessage(counter) {
   if (counter > 150) {
       return "Fühlt sich an wie an der Fasnacht, nur ohne Musik und Kostüme.";
@@ -131,6 +131,7 @@ function getCrowdMessage(counter) {
   }
 }
 
+//Besucherzahlen in Markerfarbe übersetzen
 function getCrowdMarker(counter) {
   if (counter > 150) {
       return {icon: red_marker};
@@ -141,8 +142,7 @@ function getCrowdMarker(counter) {
   }
 }
 
-
-// OpenStreetMap-Tiles laden
+// Karte laden
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
   attribution: 'CartoDB Dark Matter'
 }).addTo(map);
@@ -158,8 +158,6 @@ L.marker([47.058341871115616, 8.310841259645938], getCrowdMarker(loewendenkmal.c
 L.marker([47.0580694107522, 8.311013370717475], {icon: loewendenkmalIcon }).addTo(map);
 
 
-
-
 L.marker([47.05549754312702, 8.310256739922227], getCrowdMarker(hertensteinstrasse.counter)).addTo(map)
 .bindPopup(`
   <div class="popup-content">
@@ -168,7 +166,6 @@ L.marker([47.05549754312702, 8.310256739922227], getCrowdMarker(hertensteinstras
     </div>`);
 
 L.marker([47.05520389812051, 8.31030314438318], { icon: hertensteinstrasseIcon }).addTo(map);
-
 
 
 L.marker([47.05361289724891, 8.308787900487744], getCrowdMarker(schwanenplatz.counter)).addTo(map)
@@ -181,7 +178,6 @@ L.marker([47.05361289724891, 8.308787900487744], getCrowdMarker(schwanenplatz.co
 L.marker([47.053326495141825, 8.308983862796921], {icon: schwanenplatzIcon}).addTo(map);
 
 
-
 L.marker([47.05233432536328, 8.307978424218748], getCrowdMarker(rathausquai.counter)).addTo(map)
 .bindPopup(`
   <div class="popup-content">
@@ -190,7 +186,6 @@ L.marker([47.05233432536328, 8.307978424218748], getCrowdMarker(rathausquai.coun
     </div>`);
 
 L.marker([47.052037509036495, 8.309074616902045], { icon: rathausquaiIcon }).addTo(map);
-
 
 
 L.marker([47.0516, 8.3075], getCrowdMarker(kapellbruecke.counter)).addTo(map)
